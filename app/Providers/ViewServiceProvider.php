004540178f091dca2f74205aeb03c2935c98be8d
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-21 09:27:23 +0700

    generate view service provider

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Bill;
+use App\Models\Role;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        View::composer('*', function ($view) {
+            if (Auth::check() && Auth::user()->hasAnyRole([
+                Role::RESELLER_ADMIN,
+                Role::RESELLER_OWNER,
+            ])) {
+                $totalOutstandingBill = Bill::select(DB::raw('count(id) as total'))
+                    ->whereHas('reseller', function ($q) {
+                        $q->where('user_id', Auth::id());
+                    })->whereNull('payed_at')->whereNull('accepted_at')->first()->total ?? 0;
+                $totalPaidBill = Bill::select(DB::raw('count(id) as total'))
+                    ->whereHas('reseller', function ($q) {
+                        $q->where('user_id', Auth::id());
+                    })->whereNotNull('payed_at')->whereNull('accepted_at')->first()->total ?? 0;
+            }
+
+            if (Auth::check() && Auth::user()->hasRole(Role::CLIENT)) {
+                $totalOutstandingBill = Bill::select(DB::raw('count(id) as total'))
+                    ->whereHas('client.user', function ($q) {
+                        $q->where('id', Auth::id());
+                    })->whereNull('payed_at')->whereNull('accepted_at')->first()->total ?? 0;
+                $totalPaidBill = Bill::select(DB::raw('count(id) as total'))
+                    ->whereHas('client.user', function ($q) {
+                        $q->where('id', Auth::id());
+                    })->whereNotNull('payed_at')->whereNull('accepted_at')->first()->total ?? 0;
+            }
+
+            $view->with('totalOutstandingBill', $totalOutstandingBill ?? 0);
+            $view->with('totalPaidBill', $totalPaidBill ?? 0);
+        });
+    }
+}
