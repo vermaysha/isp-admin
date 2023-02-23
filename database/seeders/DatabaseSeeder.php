ecdb733d3be818512096f3dfe926f1afaeb0ecb0
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-23 13:12:01 +0700

    Add InvoicePDFSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             ClientSeeder::class,
             BillSeeder::class,
             WalletSeeder::class,
+            InvoicePDFSeeder::class,
         ]);
     }
 }
