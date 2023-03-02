d8cdd2d5ed7e779e7fea466d6c092b07f032fb72
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-02 21:15:08 +0700

    Remove Core UI styles and scripts from admin home page

--- resources/views/pages/admin/home.blade.php
+++ resources/views/pages/admin/home.blade.php
@@ -4,21 +4,6 @@
     {{ $title ?? 'Dasboard' }}
 @endsection
 
-@section('stylesheet')
-    <link rel="stylesheet" href="https://unpkg.com/@coreui/icons/css/all.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/@coreui/icons/css/free.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/@coreui/icons/css/brand.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/@coreui/icons/css/flag.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@coreui/chartjs@3.0.0/dist/css/coreui-chartjs.min.css">
-@endsection
-
-@section('script')
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.1.2/dist/chart.umd.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@coreui/chartjs@3.0.0/dist/js/coreui-chartjs.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@coreui/utils@1.3.1/dist/coreui-utils.js"></script>
-    <script src="js/main.js"></script>
-@endsection
-
 @section('content')
     <div class="container-lg">
         <div class="row mb-4">
