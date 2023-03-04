79647abeafb30f9c39ca267592b6d0b749b5fad0
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-04 08:08:34 +0700

    Add example files to storage disks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,6 +23,12 @@ class DatabaseSeeder extends Seeder
                 AdminSeeder::class,
             ]);
         } else {
+            $file = Http::get('https://placehold.jp/ffffff/000000/200x300.jpg?text=Contoh%20File%20KTP');
+            Storage::disk('ktp')->put('example.jpg', $file->body());
+
+            $file = Http::get('https://placehold.jp/ffffff/000000/200x300.jpg?text=Contoh%20File%20Kontrak');
+            Storage::disk('contracts')->put('example.jpg', $file->body());
+
             $this->call([
                 RoleSeeder::class,
                 AdminSeeder::class,
