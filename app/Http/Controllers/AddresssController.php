ba4cd2f0e470c7682db412b9ae96a14be70c63ab
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-05 06:44:31 +0700

    Update AddresssController to select additional fields for provinces, cities, districts and villages

--- app/Http/Controllers/AddresssController.php
+++ app/Http/Controllers/AddresssController.php
@@ -17,7 +17,7 @@ class AddresssController extends Controller
      */
     public function provinces(Request $request): JsonResponse
     {
-        $provinces = Province::select(['code', 'name']);
+        $provinces = Province::select('id', 'code', 'name');
 
         if ($request->has('name') && is_null($request->input('name')) === false) {
             $provinces->where('name', 'like', '%' . $request->input('name') . '%');
@@ -31,7 +31,7 @@ class AddresssController extends Controller
      */
     public function cities(Request $request, string $provinceCode): JsonResponse
     {
-        $city = City::select('code', 'name')->where('province_code', $provinceCode);
+        $city = City::select('id', 'code', 'name')->where('province_code', $provinceCode);
 
         if ($request->has('name') && is_null($request->input('name')) === false) {
             $city->where('name', 'like', '%' . $request->input('name') . '%');
@@ -45,7 +45,7 @@ class AddresssController extends Controller
      */
     public function districts(Request $request, string $cityCode): JsonResponse
     {
-        $districts = District::select('code', 'name')->where('city_code', $cityCode);
+        $districts = District::select('id', 'code', 'name')->where('city_code', $cityCode);
 
         if ($request->has('name') && is_null($request->input('name')) === false) {
             $districts->where('name', 'like', '%' . $request->input('name') . '%');
@@ -59,7 +59,7 @@ class AddresssController extends Controller
      */
     public function villages(Request $request, string $districtCode): JsonResponse
     {
-        $villages = Village::select('code', 'name')->where('district_code', $districtCode);
+        $villages = Village::select('id', 'code', 'name')->where('district_code', $districtCode);
 
         if ($request->has('name') && is_null($request->input('name')) === false) {
             $villages->where('name', 'like', '%' . $request->input('name') . '%');
