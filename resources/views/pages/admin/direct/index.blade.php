5e0fa14674fcc11494b695c1138af8a796931856
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-14 18:57:21 +0700

    Update tabel daftar kantor dan detailnya

--- resources/views/pages/admin/direct/index.blade.php
+++ resources/views/pages/admin/direct/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('pageTitle')
-    {{ $title ?? 'Pelanggan Direct' }}
+    {{ $title ?? 'Kantor' }}
 @endsection
 
 @section('content')
@@ -9,50 +9,49 @@
         <div class="row g-0">
             <div class="card">
                 <div class="card-header">
-                    <strong>Pelanggan Direct</strong>
+                    <strong>Kantor</strong>
                 </div>
                 <div class="card-body py-4">
                     <div class="table-responsive px-3">
                         <table class="table table-hover align-middle custom-table">
                             <thead>
                                 <tr>
-                                    <th scope="col">#</th>
                                     <th scope="col">Kantor</th>
-                                    <th scope="col">Admin</th>
+                                    <th scope="col">Akun Pemilik</th>
                                     <th scope="col">Pelanggan</th>
+                                    <th scope="col">Kota</th>
                                 </tr>
                             </thead>
                             <tbody class="table-group-divider">
-                                <tr>
-                                    <th> <img src="{{ mix('assets/brand/GMDP_full.png') }}" style="width: 45px"> </th>
-                                    <th> <a href="{{ route('admin.clientMenu.office') }}">SOLO</a> </th>
-                                    <th>
-                                        <span class="badge rounded-pill bg-primary">5 Admin</span>
-                                    </th>
-                                    <th>
-                                        <span class="badge rounded-pill bg-info">700 Pelanggan</span>
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th> <img src="{{ mix('assets/brand/GMDP_full.png') }}" style="width: 45px"> </th>
-                                    <th> <a href="{{ route('admin.clientMenu.office') }}">TANGGERANG</a> </th>
-                                    <th>
-                                        <span class="badge rounded-pill bg-primary">5 Admin</span>
-                                    </th>
-                                    <th>
-                                        <span class="badge rounded-pill bg-info">700 Pelanggan</span>
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th> <img src="{{ mix('assets/brand/GMDP_full.png') }}" style="width: 45px"> </th>
-                                    <th> <a href="{{ route('admin.clientMenu.office') }}">GRESIK</a> </th>
-                                    <th>
-                                        <span class="badge rounded-pill bg-primary">5 Admin</span>
-                                    </th>
-                                    <th>
-                                        <span class="badge rounded-pill bg-info">700 Pelanggan</span>
-                                    </th>
-                                </tr>
+                                @foreach ($offices as $office)
+                                    <tr>
+                                        <th>
+                                            <a href="{{ route('admin.officeMenu.detail', ['id' => $office->id]) }}">
+                                                <img alt="Logo GMDP" src="{{ mix('assets/brand/GMDP_full.png') }}"
+                                                    class="img-thumbnail rounded-circle" style="width: 60px">
+                                                <span class="ms-2">{{ $office->name }}</span>
+                                            </a>
+                                        </th>
+                                        <th>
+                                            @foreach ($office->reseller->employees as $employee)
+                                                @if ($employee->hasRole('Reseller_Owner'))
+                                                    <a
+                                                        href="{{ route('admin.userMenu.detail', ['id' => $employee->id]) }}">{{ $employee->fullname }}</a>
+                                                @else
+                                                    -
+                                                @endif
+                                            @endforeach
+                                        </th>
+                                        <th>
+                                            <span
+                                                class="badge rounded-pill bg-info">{{ $office->reseller->clients_count ?? '0' }}
+                                                Pelanggan</span>
+                                        </th>
+                                        <th>
+                                            {{ $office->address->city->name }}
+                                        </th>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
